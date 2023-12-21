4a11f424a5d520f8c84d10a66c9ecc93d5f9426c
Otso Rouhiainen <otso.rouhiainen@gmail.com>
2023-12-22 01:43:04 +0200

    2.9 search people by name

--- osa2/phonebook/src/App.jsx
+++ osa2/phonebook/src/App.jsx
@@ -3,10 +3,15 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNumber] = useState('')
+  const [newFilter, setFilter] = useState('')
+  const [showAll, setShowAll] = useState(true)
 
   const addPerson = (event) =>{
     event.preventDefault()
@@ -17,7 +22,8 @@ const App = () => {
     }else{
       const personObject = {
         name: newName,
-        number: newNumber
+        number: newNumber,
+        id: persons.length+1
       }
       setPersons(persons.concat(personObject))
       setNewName('')
@@ -32,11 +38,23 @@ const App = () => {
     event.preventDefault()
     setNumber(event.target.value)
   }
+  const handleFilterChange = (event) =>{
+    event.preventDefault()
+    setFilter(event.target.value)
+  }
 
+  const personToShow = !showAll
+    ? persons
+    : persons.filter(person => person.name.toLowerCase().includes(newFilter.toLowerCase()))
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+          filter shown with <input value={newFilter} onChange={handleFilterChange} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
+        
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
         </div>
@@ -49,8 +67,8 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person => 
-          <li key={person.name}>{person.name} {person.number}</li>
+        {personToShow.map(person => 
+          <li key={person.id}>{person.name} {person.number}</li>
         )}
       </ul>
     </div>
