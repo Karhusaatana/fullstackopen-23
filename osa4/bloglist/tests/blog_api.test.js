d15be547a6808dd6bc39b94575acee96a7587b6d
cxotro <otso.rouhiainen@tuni.fi>
2024-07-15 12:18:38 +0300

    4.8 bloglist tests step1

---
+++ osa4/bloglist/tests/blog_api.test.js
@@ -0,0 +1,48 @@
+const { test, after, beforeEach } = require('node:test')
+const Blog = require('../models/blog')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+
+const initialBlogs = [
+  {
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+    likes: 5
+  },
+  {
+    title: 'Monkey mode',
+    author: 'Tossavainen',
+    url: 'blaablaa.pdf',
+    likes: 4
+  }
+]
+
+beforeEach(async () =>{
+  await Blog.deleteMany({})
+  let blogObject = new Blog(initialBlogs[0])
+  await blogObject.save()
+  blogObject = new Blog(initialBlogs[1])
+  await blogObject.save()
+})
+
+test('there are two blogs', async () => {
+  const response = await api.get('/api/blogs')
+
+  assert.strictEqual(response.body.length, initialBlogs.length)
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
