346e5f95652a6677298d83aa6c6da343a509f87d
cxotro <otso.rouhiainen@tuni.fi>
2024-08-07 09:34:57 +0300

    5.8

--- osa5/bloglist-frontend/src/components/Blog.jsx
+++ osa5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,35 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  if(!visible){
+    return(
+      <div style={blogStyle}>
+        {blog.title} {blog.author} <button onClick={toggleVisibility}>view</button>
+      </div>
+    )
+  } else {
+    return (
+      <div style={blogStyle}>
+        {blog.title} {blog.author} <button onClick={toggleVisibility}>hide</button>
+        <div>{blog.url}</div>
+        <div>likes {blog.likes} <button>like</button></div>
+      </div>  
+    )
+  }
+}
 
 export default Blog
